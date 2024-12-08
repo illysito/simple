a680050c2416dbfa48afd00f66a9be4320f88963
illysito <illyalukyanovdesign@gmail.com>
2024-12-08 20:57:57 +0000

    letteres recovered

--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
 import './styles/style.css'
 
 console.log(
   'El server esta apagado y esta mierda esta funcionando en REMOTO HERMANO'
 )
+
+gsap.registerPlugin(ScrollTrigger)
+
+const h = document.querySelectorAll('.hero-h')
+const hero = document.querySelector('.hero')
+
+gsap.to(h, {
+  y: 250,
+  scrollTrigger: {
+    trigger: hero,
+    start: 'top top',
+    end: 'bottom top',
+    scrub: 1,
+  },
+})
